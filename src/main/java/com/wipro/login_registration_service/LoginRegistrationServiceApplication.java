f4f4efa03ad383c7e29d80a5f85348cf87f91f84
Yugendran <yugendrana17@gmail.com>
2024-09-24 11:49:10 +0530

    Initial Commit

---
+++ src/main/java/com/wipro/login_registration_service/LoginRegistrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wipro.login_registration_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRegistrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginRegistrationServiceApplication.class, args);
+	}
+
+}
